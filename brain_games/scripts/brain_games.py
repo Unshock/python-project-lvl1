b4b2fec70c0b10ee9f8f82e458f2eacca60a74f1
Nedashkovsky <Nedashkovsky@ymail.com>
2022-02-07 21:37:43 +0300

    add cli.py mod brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+import brain_games.cli as cli
+
 def greet():
     print("Welcome to the Brain Games!")
+    cli.welcome_user
 
 def main():
     greet()
+    cli.welcome_user()
 
 if __name__=='__main__':
     main()
