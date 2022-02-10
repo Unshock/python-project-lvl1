8c20e92e68bdc3e74ddf1bd1744bd7deb5a1978f
Nedashkovsky <Nedashkovsky@ymail.com>
2022-02-10 21:52:51 +0300

    add new game brain_even.py

---
+++ brain_games/games/brain_even.py
@@ -0,0 +1,48 @@
+#игра "Проверка на четность"
+import prompt
+from random import randint
+
+
+def greet():
+    print("Welcome to the Brain Games!")
+
+
+def welcome_user():
+    global name
+    name = prompt.string('May I have your name? ')
+    print('Hello, ' + name)
+    return name
+
+
+def is_even():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    i = 1
+    global name
+
+    while i < 4:
+        question_number = randint(1, 100)
+        if question_number % 2 == 0:
+            right_answer = 'yes'
+        else:
+            right_answer = 'no'
+
+        print('Question: ' + str(question_number))
+        answer = prompt.string('Your answer: ')
+
+        if answer == right_answer:
+            print('Correct!')
+            i += 1
+        else:
+            print("'{}' is wrong answer ;(. Correct answer was '{}'.\nLet's try again, {}!".format(answer, right_answer, name))
+            return
+    print('Congratulations, ' + name + '!')
+
+
+def main():
+    greet()
+    welcome_user()
+    is_even()
+
+if __name__ == '__main__':
+    main()
