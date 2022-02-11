b8a53b609a4a3a17d680758805eb00e1f0e35dd4
Nedashkovsky <Nedashkovsky@ymail.com>
2022-02-11 23:45:43 +0300

    mod brain_even and add to scripst

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,36 @@
+# игра "Проверка на четность"
+
+import prompt
+from brain_games.games.brain_even_logic import game_logic
+from brain_games.cli import welcome_user
+from brain_games.greet import greet
+from brain_games.answer_check import answer_check
+
+def game():
+    name = welcome_user()
+    
+    question_string, question, right_answer = game_logic()
+    
+    print(str(question_string))
+
+    i = 1
+
+    while i < 4:
+      
+        question_string, question, right_answer= game_logic()
+        print('Question: ' + str(question))
+        answer = prompt.string('Your answer: ')
+        if answer_check(answer, right_answer, name, i) == True:
+            i += 1
+        else: return
+        
+    print('Congratulations, ' + name + '!')
+
+
+def main():
+    greet()
+    game()
+
+
+if __name__ == '__main__':
+    main()
