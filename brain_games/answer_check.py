7b600043f9d5365617183ae6077be73d88380839
Nedashkovsky <Nedashkovsky@ymail.com>
2022-02-11 23:44:06 +0300

    add files answer_check.py and greet.py

---
+++ brain_games/answer_check.py
@@ -0,0 +1,9 @@
+def answer_check(answer, right_answer, name, i):
+    if answer == str(right_answer):
+        print('Correct!')
+        return True
+    else:
+        print("'{}' is wrong answer ;(. Correct answer was '{}'.\n"
+              "Let's try again, {}!".format(answer, str(right_answer), str(name)))
+        return False
+
---
+++ brain_games/greet.py
@@ -0,0 +1,3 @@
+def greet():
+    print("Welcome to the Brain Games!")
+
